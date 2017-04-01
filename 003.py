48a3713a2d74536fe8bb097e0ab3deda579f51fc
HeeChan <heech.kang@gmail.com>
2017-04-01 09:44:17 +0630

    Added solution to 003.

---
+++ 003.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        resulting_string = []
+	maxlength = 0
+	    
+	for i in range(0,len(s)):
+	    if s[i] not in resulting_string:
+		resulting_string.append(s[i])
+		if len(resulting_string) > maxlength:
+		    maxlength = len(resulting_string)
+	    else:
+		newindex = resulting_string.index(s[i]) + 1
+		resulting_string = resulting_string[newindex:]
+		resulting_string.append(s[i])
+	return maxlength
