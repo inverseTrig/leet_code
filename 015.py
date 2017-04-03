1d623d295f3da72816225baa92a45211352e365c
HeeChan <heech.kang@gmail.com>
2017-04-03 15:55:12 +0630

    Solved problem number 15.

---
+++ 015.py
@@ -0,0 +1,28 @@
+# 3Sum
+
+class Solution(object):
+    def threeSum(self, nums):
+	
+	nums.sort()
+	sumSolution = []
+	
+	for i in xrange(len(nums)-2):
+	    if i > 0 and nums[i] == nums[i-1]:
+		continue
+		
+	    j, k = i + 1, len(nums) - 1
+	    
+	    while j < k:
+		s = nums[i] + nums[j] + nums[k]
+		if s < 0:
+		    j += 1
+		elif s > 0:
+		    k -= 1
+		else:
+		    sumSolution.append((nums[i], nums[j], nums[k]))
+		    while j < k and nums[j] == nums [j + 1]:
+			j += 1
+		    while j > k and nums[k] == nums[k - 1]:
+			r -= 1
+		    j += 1; k -= 1
+	return sumSolution
