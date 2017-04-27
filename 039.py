3dc91babe10c6544a2f67c3d6ea7bae54b0f8969
HeeChan <heech.kang@gmail.com>
2017-04-27 17:01:15 +0630

    Added problem 39.

---
+++ 039.py
@@ -0,0 +1,25 @@
+# Combination Sum
+
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        combi = []
+	candidates.sort()
+	self.dfs(candidates, target, 0, [], combi)
+	return combi
+	
+    def dfs(self, nums, target, index, path, combi):
+	if target < 0:
+	    return
+	if target == 0:
+	    combi.append(path)
+	    return
+	for i in xrange(index, len(nums)):
+	    self.dfs(nums, target - nums[i], i, path+[nums[i]], combi)
+
+
+print(Solution().combinationSum([2, 3, 6, 7], 7))
