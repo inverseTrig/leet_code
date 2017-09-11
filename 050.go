643b78dd4fe3b98c2da463aa55c03a1efe81d45f
HeeChan Kang <heech.kang@gmail.com>
2017-09-11 10:42:14 -0500

    Solution to problem 50.

---
+++ 050.go
@@ -0,0 +1,20 @@
+// Pow(x,n)
+
+package main
+
+func main() {
+
+}
+
+func myPow(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	} else if n < 0 {
+		n = -n
+		x = 1 / x
+	}
+	if n%2 == 0 {
+		return myPow(x*x, n/2)
+	}
+	return x * myPow(x*x, n/2)
+}
