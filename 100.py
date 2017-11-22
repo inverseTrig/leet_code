9208ba82ef20114102e36ac84c846b92f587c2a6
HeeChan Kang <heech.kang@Gmail.com>
2017-11-21 18:22:56 -0600

    Adding solution to problem number 100, Same Tree.

---
+++ 100.py
@@ -0,0 +1,7 @@
+# Same Tree
+
+class Solution(object):
+	def isSameTree(self, p, q):
+		if p and q:
+			return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+		return p is q
